72cd348eea3bd131d9f85ee333a7e1ff35e02d69
nguyentuanduc <nguyentuanduc18@gmai.com>
2019-11-03 23:27:56 +0700

    first commit

---
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -0,0 +1,83 @@
+package com.springboot.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.springboot.exception.ResourceNotFoundException;
+import com.springboot.model.Note;
+import com.springboot.repository.NoteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;  
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+    @RequestMapping(value="/index" , method=RequestMethod.GET)  
+    public String index() {
+        return "index";
+    }
+
+
+    @RequestMapping(value="/save", method=RequestMethod.GET)  
+    public ModelAndView save(){  
+        ModelAndView modelAndView = new ModelAndView();  
+        modelAndView.setViewName("index");      
+        return modelAndView;  
+    }
+
+    @RequestMapping(value="/listNode" , method=RequestMethod.GET)
+    public String listNode(){
+        return "listNode";
+    }
+
+    @GetMapping("/notes")
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    @PostMapping("/notes")
+    public Note createNote(@Valid @RequestBody Note note) {
+        return noteRepository.save(note);
+    }
+
+    @GetMapping("/notes/{id}")
+    public Note getNoteById(@PathVariable(value = "id") Long noteId) {
+        return noteRepository.findById(noteId).orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+    }
+    
+    @PutMapping("/notes/{id}")
+    public Note updateNote(@PathVariable(value = "id") Long noteId, @Valid @RequestBody Note noteDetails) {
+        Note note = noteRepository.findById(noteId).orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+        note.setTitle(noteDetails.getTitle());
+        note.setContent(noteDetails.getContent());
+    
+        Note updatedNote = noteRepository.save(note);
+        return updatedNote;
+    }
+    
+    @DeleteMapping("/notes/{id}")
+    public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long noteId) {
+        Note note = noteRepository.findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+
+        noteRepository.delete(note);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
